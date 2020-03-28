16f489907a802176632ab3e6efa9fee7355263a5
cawfree <hello@cawfree.com>
2020-03-28 16:27:08 +0000

    Verified simple plugin utilization.

--- src/js/popup/example.js
+++ src/js/popup/example.js
@@ -1,3 +1,5 @@
+import { nanoid } from "nanoid";
+
 export default function () {
-  alert("hello! (find me on src/js/popup/example.js)");
+  alert("hello! (find me on src/js/popup/example.js) " + nanoid());
 };
