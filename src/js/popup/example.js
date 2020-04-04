e86262d68563c3b9733a0722a79cb2c6f266841d
cawfree <hello@cawfree.com>
2020-04-04 20:29:37 +0100

    Basic integration of React.

--- src/js/popup/example.js
+++
@@ -1,5 +0,0 @@
-import { nanoid } from "nanoid";
-
-export default function () {
-  alert("hello! (find me on src/js/popup/example.js) " + nanoid());
-};
--- webpack.config.js
+++ webpack.config.js
@@ -1,93 +1,39 @@
-var webpack = require("webpack"),
-    path = require("path"),
-    fileSystem = require("fs"),
-    env = require("./config/env"),
-    CleanWebpackPlugin = require("clean-webpack-plugin").CleanWebpackPlugin,
-    CopyWebpackPlugin = require("copy-webpack-plugin"),
-    HtmlWebpackPlugin = require("html-webpack-plugin"),
-    WriteFilePlugin = require("write-file-webpack-plugin");
+const path = require('path');
+const CopyPlugin  = require('copy-webpack-plugin');
 
-// load the secrets
-var alias = {};
-
-var secretsPath = path.join(__dirname, ("secrets." + env.NODE_ENV + ".js"));
-
-var fileExtensions = ["jpg", "jpeg", "png", "gif", "eot", "otf", "svg", "ttf", "woff", "woff2"];
-
-if (fileSystem.existsSync(secretsPath)) {
-  alias["secrets"] = secretsPath;
-}
-
-var options = {
-  mode: process.env.NODE_ENV || "development",
-  entry: {
-    popup: path.join(__dirname, "src", "js", "popup.js"),
-    options: path.join(__dirname, "src", "js", "options.js"),
-    background: path.join(__dirname, "src", "js", "background.js")
-  },
+module.exports = {
+  entry: './extension/src/index.js',
   output: {
-    path: path.join(__dirname, "build"),
-    filename: "[name].bundle.js"
+    filename: 'bundle.js',
+    path: path.resolve(__dirname, 'dist'),
   },
   module: {
     rules: [
       {
-        test: /\.css$/,
-        loader: "style-loader!css-loader",
-        exclude: /node_modules/
+        test: /\.mp3/i,
+        use: 'file-loader',
       },
       {
-        test: new RegExp('.(' + fileExtensions.join('|') + ')$'),
-        loader: "file-loader?name=[name].[ext]",
-        exclude: /node_modules/
+        test: /\.m?js$/,
+        exclude: /(node_modules|bower_components)/,
+        use: {
+          loader: 'babel-loader',
+          options: {
+            presets: ['@babel/preset-env', '@babel/preset-react']
+          }
+        }
       },
-      {
-        test: /\.html$/,
-        loader: "html-loader",
-        exclude: /node_modules/
-      }
-    ]
+    ],
   },
-  resolve: {
-    alias: alias
+  optimization: {
+    minimize: false,
   },
   plugins: [
-    // clean the build folder
-    new CleanWebpackPlugin(),
-    // expose and write the allowed env vars on the compiled bundle
-    new webpack.EnvironmentPlugin(["NODE_ENV"]),
-    new CopyWebpackPlugin([{
-      from: "src/manifest.json",
-      transform: function (content, path) {
-        // generates the manifest file using the package.json informations
-        return Buffer.from(JSON.stringify({
-          description: process.env.npm_package_description,
-          version: process.env.npm_package_version,
-          ...JSON.parse(content.toString())
-        }))
-      }
-    }]),
-    new HtmlWebpackPlugin({
-      template: path.join(__dirname, "src", "popup.html"),
-      filename: "popup.html",
-      chunks: ["popup"]
-    }),
-    new HtmlWebpackPlugin({
-      template: path.join(__dirname, "src", "options.html"),
-      filename: "options.html",
-      chunks: ["options"]
-    }),
-    new HtmlWebpackPlugin({
-      template: path.join(__dirname, "src", "background.html"),
-      filename: "background.html",
-      chunks: ["background"]
-    }),
-    new WriteFilePlugin()
-  ]
+    new CopyPlugin([
+      { from: './extension/index.js', to: './index.js' },
+      { from: './extension/icon.png', to: './icon.png' },
+      { from: './extension/index.html', to: './index.html' },
+      { from: './extension/manifest.json', to: './manifest.json' },
+    ]),
+  ],
 };
-
-if (env.NODE_ENV === "development") {
-  options.devtool = "cheap-module-eval-source-map";
-}
-
-module.exports = options;
