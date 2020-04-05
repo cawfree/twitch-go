f1d14273bba21aae11f916b29c861b34f76831c9
cawfree <hello@cawfree.com>
2020-04-06 00:11:26 +0100

    Seemingly successful invocation of ffmpeg

--- extension/src/index.js
+++ extension/src/index.js
@@ -1,37 +1,14 @@
+import "@babel/polyfill";
+
 import React from "react";
 import ReactDOM from "react-dom";
-import ffmpeg from "ffmpeg.js";
+import ffmpeg from "ffmpeg.js/ffmpeg-webm.js";
 
 ReactDOM.render(
   <>hello</>,
   document.getElementById("root"),
 );
 
-/*
-Copyright 2014 Intel Inc.
-
-Licensed under the Apache License, Version 2.0 (the "License");
-you may not use this file except in compliance with the License.
-You may obtain a copy of the License at
-
-     http://www.apache.org/licenses/LICENSE-2.0
-
-Unless required by applicable law or agreed to in writing, software
-distributed under the License is distributed on an "AS IS" BASIS,
-WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
-See the License for the specific language governing permissions and
-limitations under the License.
-
-Author: Dongseong Hwang (dongseong.hwang@intel.com)
-*/
-
-/**
- * Grabs the desktop capture feed from the browser, requesting
- * desktop capture. Requires the permissions
- * for desktop capture to be set in the manifest.
- *
- * @see https://developer.chrome.com/apps/desktopCapture
- */
 var desktop_sharing = false;
 var local_stream = null;
 function toggle() {
@@ -74,80 +51,56 @@ function onAccessApproved(desktop_id) {
 }
 
 function gotStream(stream) {
-    local_stream = stream;
-    document.querySelector('video').srcObject = stream;
+  local_stream = stream;
+  const videoElem = document.querySelector('video');
+  videoElem.srcObject = stream;
 
-    var videoStream = document.querySelector('video').captureStream();
+  const options = {mimeType: 'video/webm;codecs=h264'}; 
+  const mediaRecorder = new MediaRecorder(stream, options);
+  const recordedBlobs = [];
 
-    let mediaRecorder = new MediaRecorder(stream);
-    mediaRecorder.onstop = () => console.log('got stop');
-    mediaRecorder.start(5000);
+  mediaRecorder.onstop = async () => {
+    console.log('got stop');
+    const blob = new Blob(recordedBlobs, options);
+    // XXX: Convert into Uint8.
+    const data = await blob.arrayBuffer();
+
+    console.log('webm video is',blob);
+    console.log('data is',data);
 
     var stdout = "";
-    var stderr = ""; 
+    var stderr = "";
 
-    mediaRecorder.ondataavailable = (event) => {
-      console.log('got data');
-      console.log(event);
-      if (event.data && event.data.size > 0) {
-        console.log('got good data');
-        // ffmpeg -f x11grab -s 1920x1200 -framerate 15 -i :0.0 -c:v libx264 -preset fast -pix_fmt yuv420p -s 1280x800 -threads 0 -f flv "rtmp://live.twitch.tv/app/live_********_******************************
-        ffmpeg({
-          MEMFS: [{
-            name: "test.mp4",
-            data: new Blob([event.data], { type: 'video/mp4' }),
-          }],
-          arguments: [
-            "-f",
-            "test.mp4",
-            "-s",
-            "1920x1200",
-            "-framerate",
-            "15",
-            "-c:v",
-            "libx264",
-            //"-preset",
-            //"fast",
-            "-pix_fmt",
-            "yuv420p",
-            "-s",
-            "1280x800",
-            "-threads",
-            "0",
-            "-f",
-            "flv",
-            "\"rtmp://live-lhr03.twitch.tv/app/live_106182096_kBSohhfylG2nCWSQSLX1nWACmsgDNp\"",
-            // --enable-demuxer=flv
-            "--enable-demuxer=flv",
-            //"flv",
-            //"--enable-muxer=flv"
-          ],
-          //arguments: ["-version"],
-          print: function(data) { stdout += data + "\n"; },
-          printErr: function(data) { stderr += data + "\n"; },
-          onExit: function(code) {
-            console.log("Process exited with code " + code);
-            console.log(stdout);
-            console.log(stderr);
-          },
-        });
-      }
-    };
-
-    stream.onended = function() {
-      if (desktop_sharing) {
-        toggle();
-      }
+    const result = ffmpeg({
+      MEMFS: [{name: "test.webm", data }],
+      arguments: ["-i", "test.webm", "-c:v", "libvpx", "-an", "out.webm"],
+      print: function(data) { stdout += data + "\n"; },
+      printErr: function(data) { stderr += data + "\n"; },
+      onExit: function(code) {
+        console.log("Process exited with code " + code);
+        console.log(stdout);
+        console.log(stderr);
+      },
+    });
+  };
+  mediaRecorder.ondataavailable = (event) => {
+    console.log('got data');
+    if (event.data && event.data.size > 0) {
+      recordedBlobs.push(event.data);
     }
-  }
+  };
+  console.log('starting');
+  mediaRecorder.start(1000);
+  setTimeout(
+    () => mediaRecorder.stop(),
+    1000,
+  );
+}
 
 function getUserMediaError(e) {
   console.log('getUserMediaError: ' + JSON.stringify(e, null, '---'));
 }
 
-/**
- * Click handler to init the desktop capture grab
- */
 document.querySelector('button').addEventListener('click', function(e) {
   toggle();
 });
